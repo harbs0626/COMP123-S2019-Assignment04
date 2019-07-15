f1458ae7a03a94251fc745e89a2ed7877e7928e3
Harbin Ramo <hramo@my.centennialcollege.ca>
2019-07-15 13:48:54 -0400

    Added initial summary of the app

--- BMICalculator/BMICalculatorForm.cs
+++ BMICalculator/BMICalculatorForm.cs
@@ -7,7 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/// <summary>
+/// 
+/// Application Name:   BMI Calculator App
+/// 
+/// Student Name:       Harbin Ramo
+/// Student Number:     301046044
+/// 
+/// BMI Scale:
+/// ----------
+/// Underweight     Less than 18.5
+/// Normal          Between 18.5 and 24.9
+/// Overweight      Between 25 and 29.9
+/// Obese           30 or greater
+/// 
+/// BMI Formula:
+/// ------------
+/// BMI =           weightInPounds * 703
+///           -------------------------------
+///           heightInInches * heightInInches
+///           
+/// BMI =             weightInKilograms
+///           -------------------------------
+///           heightInMeters * heightInMeters
+///           
+/// </summary>
 namespace BMICalculator
 {
     public partial class BMICalculatorForm : Form
